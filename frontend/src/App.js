12e3261821e08f5289a4146da532de946db3c414
Stefan <57998509+stefanshr@users.noreply.github.com>
2021-10-16 01:37:19 +0200

    frontend
    Signed-off-by: Stefan <57998509+stefanshr@users.noreply.github.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+function useForceUpdate() {
+    let [value, setState] = useState(true);
+    return () => setState(!value);
+}
+
+let amount;
+export default function App() {
+    let forceUpdate = useForceUpdate();
+
+    async function load() {
+        const result = await axios.get('http://localhost:8080/api/getAmount');
+        amount = result.data
+        forceUpdate()
+
+
+
+    }
+
+
+    window.onload = function () {
+        load()
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>This page was viewed this many times:</h1>
+                <h2>{amount}</h2>
+            </header>
+        </div>
+    );
+}
