43210ac332a6646eb2f18eb08d548a8d0d1ec8a2
Stefan <57998509+stefanshr@users.noreply.github.com>
2021-10-20 22:31:11 +0200

    update
    Signed-off-by: Stefan <57998509+stefanshr@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,7 @@ export default function App() {
                     <h1>This page was viewed this many times:</h1>
                     <h2>{amount}</h2>
                 </header>
+                <footer>test</footer>
             </div>
     );
 }
