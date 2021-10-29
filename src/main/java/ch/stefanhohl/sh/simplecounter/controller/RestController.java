c89a336e81e08096f274e2c40226bb33a30c30e9
Stefan <57998509+stefanshr@users.noreply.github.com>
2021-10-30 00:16:51 +0200

    logging
    Signed-off-by: Stefan <57998509+stefanshr@users.noreply.github.com>

--- src/main/java/ch/stefanhohl/sh/simplecounter/controller/RestController.java
+++ src/main/java/ch/stefanhohl/sh/simplecounter/controller/RestController.java
@@ -1,10 +1,12 @@
 package ch.stefanhohl.sh.simplecounter.controller;
 
+import java.util.logging.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -16,11 +18,12 @@ import java.util.List;
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = "/api")
 public class RestController {
-
+    private static final Logger LOGGER = Logger.getLogger( RestController.class.getName() );
     long count = 0;
 
     @GetMapping(value = "/getAmount")
-    public long getAmount() {
-       return count++;
+    public long getAmount(HttpServletRequest request) {
+        LOGGER.info(request.getRemoteAddr());
+        return count++;
     }
 }
